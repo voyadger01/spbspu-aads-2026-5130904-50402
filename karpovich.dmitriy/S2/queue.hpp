b341516b024de13021fa691968d24e270db57daa
voyadger01 <voyadgerkx@gmail.com>
2026-05-24 12:05:52 +0300

    add move push and fixes

--- karpovich.dmitriy/S2/queue.hpp
+++ karpovich.dmitriy/S2/queue.hpp
@@ -13,8 +13,9 @@ namespace karpovich
   public:
     bool empty() const noexcept;
     size_t size() const noexcept;
-    T &front() noexcept;
+    T &front();
     void push(const T &rhs);
+    void push(T &&rhs);
     void pop() noexcept;
   };
 
@@ -23,25 +24,36 @@ namespace karpovich
   {
     return data_.empty();
   }
+
   template< class T >
   size_t Queue< T >::size() const noexcept
   {
     return data_.size();
   }
+
   template< class T >
-  T &Queue< T >::front() noexcept
+  T &Queue< T >::front()
   {
     return data_.front();
   }
+
   template< class T >
   void Queue< T >::push(const T &rhs)
   {
-    data_.pushBack(rhs);
+    data_.push_back(rhs);
   }
+
+  template< class T >
+  void Queue< T >::push(T &&rhs)
+  {
+    data_.push_back(std::move(rhs));
+  }
+
   template< class T >
   void Queue< T >::pop() noexcept
   {
-    data_.popFront();
+    data_.pop_front();
   }
 }
+
 #endif
--- karpovich.dmitriy/S2/stack.hpp
+++ karpovich.dmitriy/S2/stack.hpp
@@ -2,6 +2,7 @@
 #define STACK_HPP
 #include <cstddef>
 #include <list.hpp>
+
 namespace karpovich
 {
   template< class T >
@@ -12,34 +13,47 @@ namespace karpovich
   public:
     bool empty() const noexcept;
     size_t size() const noexcept;
-    T &top() noexcept;
+    T &top();
     void push(const T &value);
+    void push(T &&value);
     void pop() noexcept;
   };
+
   template< class T >
   bool Stack< T >::empty() const noexcept
   {
     return data_.empty();
   }
+
   template< class T >
   size_t Stack< T >::size() const noexcept
   {
     return data_.size();
   }
+
   template< class T >
-  T &Stack< T >::top() noexcept
+  T &Stack< T >::top()
   {
     return data_.back();
   }
+
   template< class T >
   void Stack< T >::push(const T &rhs)
   {
-    data_.pushBack(rhs);
+    data_.push_back(rhs);
   }
+
+  template< class T >
+  void Stack< T >::push(T &&rhs)
+  {
+    data_.push_back(std::move(rhs));
+  }
+
   template< class T >
   void Stack< T >::pop() noexcept
   {
-    data_.popBack();
+    data_.pop_back();
   }
 }
+
 #endif
