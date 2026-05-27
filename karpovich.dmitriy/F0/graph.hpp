bd462583347818bb2183eee7a3905e6fe0407afc
voyadger01 <voyadgerkx@gmail.com>
2026-05-27 15:25:31 +0300

    add remove

--- karpovich.dmitriy/F0/graph.hpp
+++ karpovich.dmitriy/F0/graph.hpp
@@ -50,6 +50,46 @@ namespace karpovich
     }
     vertices_[from].edges_.pushBack(detail::Edge< T >(to, label));
   }
+
+  template< class T >
+  void Graph< T >::removeEdge(size_t from, size_t to)
+  {
+    if (from >= vertices_.getSize()) {
+      return;
+    }
+    List< detail::Edge< T > > &edges = vertices_[from].edges_;
+    LIter< T > it = edges.begin();
+    while (it != edges.end()) {
+      if ((*it).to_ == to) {
+        it = edges.erase(it);
+      } else {
+        ++it;
+      }
+    }
+  }
+
+  template< class T >
+  void Graph< T >::removeVertex(size_t index)
+  {
+    if (index >= vertices_.getSize()) {
+      return;
+    }
+    vertices_.erase(index);
+    for (size_t i = 0; i < vertices_.getSize(); ++i) {
+      List< detail::Edge< T > > &edges = vertices_[i].edges_;
+      LIter< T > it = edges.begin();
+      while (it != edges.end()) {
+        if ((*it).to_ == index) {
+          it = edges.erase(it);
+        } else {
+          if ((*it).to_ > index) {
+            (*it).to_--;
+          }
+          ++it;
+        }
+      }
+    }
+  }
 }
 
 #endif
