67f7445e38d1420af9c5aeb524ba3d9cf1f74823
voyadger01 <voyadgerkx@gmail.com>
2026-05-28 22:04:24 +0300

    add not const iterators methods

--- karpovich.dmitriy/F0/cuckooIter.hpp
+++ karpovich.dmitriy/F0/cuckooIter.hpp
@@ -71,3 +71,98 @@ namespace karpovich
   };
 }
 
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::CuckooIter():
+  table1_(nullptr),
+  table2_(nullptr),
+  capacity_(0),
+  tableIdx_(2),
+  idx_(0)
+{}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::CuckooIter(Vector< entryType > *table1,
+                                                                     Vector< entryType > *table2, size_t capacity,
+                                                                     size_t tableIdx, size_t idx):
+  table1_(table1),
+  table2_(table2),
+  capacity_(capacity),
+  tableIdx_(tableIdx),
+  idx_(idx)
+{
+  findValid();
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+void karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::findValid()
+{
+  while (tableIdx_ < 2) {
+    Vector< entryType > *current = (tableIdx_ == 0) ? table1_ : table2_;
+
+    while (idx_ < capacity_) {
+      if ((*current)[idx_].occupied_) {
+        return;
+      }
+      ++idx_;
+    }
+
+    ++tableIdx_;
+    idx_ = 0;
+  }
+
+  table1_ = nullptr;
+  table2_ = nullptr;
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal > &
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator++()
+{
+  ++idx_;
+  findValid();
+  return *this;
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator++(int)
+{
+  CuckooIter temp(*this);
+  ++(*this);
+  return temp;
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+bool karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator==(const CuckooIter &other) const
+{
+  bool thisEnd = (table1_ == nullptr || tableIdx_ >= 2);
+  bool otherEnd = (other.table1_ == nullptr || other.tableIdx_ >= 2);
+  if (thisEnd && otherEnd) {
+    return true;
+  }
+  if (thisEnd || otherEnd) {
+    return false;
+  }
+  return tableIdx_ == other.tableIdx_ && idx_ == other.idx_;
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+bool karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator!=(const CuckooIter &other) const
+{
+  return !(*this == other);
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+typename karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::valType &
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator*() const
+{
+  Vector< entryType > *current = (tableIdx_ == 0) ? table1_ : table2_;
+  return (*current)[idx_].data_;
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+typename karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::valType *
+karpovich::CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator->() const
+{
+  return std::addressof(operator*());
+}
