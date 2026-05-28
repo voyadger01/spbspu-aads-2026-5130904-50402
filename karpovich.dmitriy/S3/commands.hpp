7189ebfa9b95e39a1fe26ed0a3acfd0cf66ae798
voyadger01 <voyadgerkx@gmail.com>
2026-05-28 20:35:40 +0300

    hasher to common

--- karpovich.dmitriy/S3/commands.hpp
+++ karpovich.dmitriy/S3/commands.hpp
@@ -1,11 +1,11 @@
 #ifndef COMMANDS_HPP
 #define COMMANDS_HPP
 
+#include <hashFunctions.hpp>
 #include <hashTable.hpp>
 #include <iostream>
 #include <string>
 #include "graph.hpp"
-#include "hashFunctions.hpp"
 
 namespace karpovich
 {
