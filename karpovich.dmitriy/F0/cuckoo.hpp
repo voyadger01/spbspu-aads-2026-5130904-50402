a0c60750bf6926140298338879c34adb4469e08b
voyadger01 <voyadgerkx@gmail.com>
2026-05-28 21:42:44 +0300

    add other methods

--- karpovich.dmitriy/F0/cuckoo.hpp
+++ karpovich.dmitriy/F0/cuckoo.hpp
@@ -232,4 +232,83 @@ const Value &karpovich::CuckooTable< Key, Value, Hash1, Hash2, Equal >::get(Key 
   throw std::out_of_range("Key not found");
 }
 
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+void karpovich::CuckooTable< Key, Value, Hash1, Hash2, Equal >::add(Key k, Value v)
+{
+  if (has(k)) {
+    get(k) = v;
+    return;
+  }
+  if (loadFactor() >= maxLoadFactor_) {
+    rehash(capacity_ * 2);
+  }
+  valType current(k, v);
+  bool firstTable = true;
+  for (size_t kick = 0; kick < maxKickCount_; ++kick) {
+    if (firstTable) {
+      size_t pos = hasher1_(current.first) % capacity_;
+      if (!table1_[pos].occupied_) {
+        table1_[pos] = entryType(current.first, current.second);
+        ++size_;
+        return;
+      }
+      std::swap(current, table1_[pos].data_);
+      firstTable = false;
+    } else {
+      size_t pos = hasher2_(current.first) % capacity_;
+      if (!table2_[pos].occupied_) {
+        table2_[pos] = entryType(current.first, current.second);
+        ++size_;
+        return;
+      }
+      std::swap(current, table2_[pos].data_);
+      firstTable = true;
+    }
+  }
+  rehash(capacity_ * 2);
+  add(current.first, current.second);
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+Value karpovich::CuckooTable< Key, Value, Hash1, Hash2, Equal >::drop(Key k)
+{
+  size_t h1 = hasher1_(k) % capacity_;
+  size_t h2 = hasher2_(k) % capacity_;
+
+  if (table1_[h1].occupied_ && comparator_(table1_[h1].data_.first, k)) {
+    table1_[h1].occupied_ = false;
+    --size_;
+    return table1_[h1].data_.second;
+  }
+  if (table2_[h2].occupied_ && comparator_(table2_[h2].data_.first, k)) {
+    table2_[h2].occupied_ = false;
+    --size_;
+    return table2_[h2].data_.second;
+  }
+  throw std::out_of_range("Key not found");
+}
+
+template< class Key, class Value, class Hash1, class Hash2, class Equal >
+void karpovich::CuckooTable< Key, Value, Hash1, Hash2, Equal >::rehash(size_t newCapacity)
+{
+  Vector< entryType > oldTable1 = table1_;
+  Vector< entryType > oldTable2 = table2_;
+  table1_ = Vector< entryType >(newCapacity);
+  table2_ = Vector< entryType >(newCapacity);
+  capacity_ = newCapacity;
+  size_ = 0;
+  maxKickCount_ = newCapacity;
+
+  for (size_t i = 0; i < oldTable1.getSize(); ++i) {
+    if (oldTable1[i].occupied_) {
+      add(oldTable1[i].data_.first, oldTable1[i].data_.second);
+    }
+  }
+  for (size_t i = 0; i < oldTable2.getSize(); ++i) {
+    if (oldTable2[i].occupied_) {
+      add(oldTable2[i].data_.first, oldTable2[i].data_.second);
+    }
+  }
+}
+
 #endif
